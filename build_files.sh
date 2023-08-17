3a09fd71f495b2fed3c9ea44e39f0b05c76997b2
safalbk <safalbk333@gmail.com>
2023-08-17 23:17:46 +0530

    migrations

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,8 @@
 echo "build start"
 pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
+
 python manage.py collectstatic --noinput --clear
+
 echo "build finished"
