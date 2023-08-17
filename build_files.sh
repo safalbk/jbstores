1c3e99daf2be5c571eb4dea9c454c824682039af
safalbk <safalbk333@gmail.com>
2023-08-17 23:25:40 +0530

    vercel build

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,6 @@
 echo "build start"
 pip install -r requirements.txt
-python manage.py makemigrations
-python manage.py migrate
 
-python manage.py collectstatic --noinput --clear
+# python manage.py collectstatic --noinput --clear
 
 echo "build finished"
